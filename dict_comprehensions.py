871f2b9eca9ef6a274900e279ac0ab617d68f158
David-prog-wed <dedcelisg@hotmail.es>
2021-04-16 18:18:08 -0500

    Reto Resuelto

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -1,3 +1,5 @@
+import math
+
 def run():
   # my_dict = {}
 
@@ -5,7 +7,9 @@ def run():
   #   if i % 3 != 0:
   #     my_dict[i] = i**3
 
-  my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+  # my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+
+  my_dict = { i: math.sqrt(i) for i in range(1,1001) }
 
   print(my_dict)
 
