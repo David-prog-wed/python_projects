f72d93060a0e1dfa1219968f23877981f9b08f5d
David celis <72824237+David-prog-wed@users.noreply.github.com>
2021-04-19 10:10:47 -0500

    haciendo merge (#10)
    * Recoletando Dates
    
    * Revelando dates de las personas adultas
    
    * Arreglo un error de sintaxis
    
    * Reto de las funciones de orden superior
    
    * Modificando sintaxis conflicto y conflicto del merge
    
    * debugging en phython
    
    * sintaxis Error
    
    * agregando mas codigo
    
    * cambio en la sintaxis
    
    * Encontrando el error con debbugging
    
    * agregando try, except y raise

--- debugging.py
+++ debugging.py
@@ -1,15 +1,19 @@
 def divisors(num):
     divisors = []
     for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
         if num % i == 1:
     return divisors
 
-
 def run():
-    num = int(input('ingresa un número: '))
-    print(divisors(num))
-    print('Termino mo programa')
-
-
+    try:
+        num = int(input('ingresa un número: '))
+        print(divisors(num))
+        print('Termino mi programa')
+    except ValueError:
+        print('Debes ingresar un número')
+      
+      
 if __name__ == '__main__':
     run()
