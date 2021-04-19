bfbe405e2af3a93cd3a370e9df727214041b8b97
David-prog-wed <dedcelisg@hotmail.es>
2021-04-19 09:16:06 -0500

    cambio en la sintaxis

--- debugging.py
+++ debugging.py
@@ -2,7 +2,7 @@ def divisors(num):
     divisors = []
     for i in range(1, num + 1):
         if num % i == 1:
-            dividors.append(i)
+            divisors.append(i)
     return divisors
 
 
