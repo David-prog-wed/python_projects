4cf64bec8a953ae2ccc68f2dadf65998cf00a4a6
David-prog-wed <dedcelisg@hotmail.es>
2021-04-20 11:14:07 -0500

    corriendo el algoritmo

--- archivos.py
+++ archivos.py
@@ -1,3 +1,14 @@
 def read():
     numbers = []
-    with open('')
+    with open('./archivos/numbers.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
+
+
+def write():
+    pass
+
+
+if __name__ == '__main__':
+    run()
