5b3b60cf4d9e3cd64794c2a2bd6c2a28e48d7f40
David celis <72824237+David-prog-wed@users.noreply.github.com>
2021-04-20 12:33:40 -0500

    'a' para agregar mas name en el archivo name.txt
    * Recoletando Dates
    
    * Revelando dates de las personas adultas
    
    * Arreglo un error de sintaxis
    
    * Reto de las funciones de orden superior
    
    * Modificando sintaxis conflicto y conflicto del merge
    
    * debugging en phython
    
    * sintaxis Error
    
    * agregando mas codigo
    
    * cambio en la sintaxis
    
    * Encontrando el error con debbugging
    
    * agregando try, except y raise
    
    * nuevo cambio
    
    * reto resuelto try_except
    
    * utilizando asser
    
    * dandole parametros a errores assert
    
    * Manejando archivos con python
    
    * agregando el archivo .txt
    
    * corriendo el algoritmo
    
    * definiendo la variable run()
    
    * nuevo archivo creado desde python
    
    * archivo modificado para caracteres especiales con utf-8
    
    * colocando cambios al archivo name.txt
    
    * colocando nuevo nombre al aerchivo name.txt
    
    * new name in name.txt characteres
    
    * Agregando 'a' para ingresar nuevas listas

--- archivos.py
+++ archivos.py
@@ -8,7 +8,7 @@ def read():
 
 def write():
     names = ['Nicolás', 'Miguel', 'Pepe', 'Cristian', 'Rocío']
-    with open('./archivos/names.txt', 'w', encoding='utf-8') as f:   
+    with open('./archivos/names.txt', 'a', encoding='utf-8') as f:   
         for name in names:
             f.write(name)
             f.write('\n')
